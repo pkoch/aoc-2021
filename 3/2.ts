65e87d9d53df7a07fd32ca38c8b16d1395b8bbae
Paulo Köch <paulo.koch@gmail.com>
2022-12-06 18:48:15 +0000

    3/2

---
+++ 3/2.ts
@@ -0,0 +1,32 @@
+import { assertNever, input_reader } from "../libtapete.ts";
+import "../langExts/Object/thrush.ts"
+import { tallyByFreq } from "./1.ts";
+
+const first = <A>(a: A[]): A => a[0];
+const last = <A>(a: A[]): A => a.at(-1)!;
+
+const finderProc = (picker: (as: string[]) => string, lines: string[], nthDigit = 0): string => {
+  if(lines.length < 1) return assertNever({picker, lines, nthDigit});
+  if(lines.length == 1) return lines[0];
+  if(nthDigit > lines[0].length) return assertNever({nthDigit});
+
+  const targetDigit = picker(tallyByFreq(lines.map(l => l[nthDigit])))
+
+  return finderProc(picker, lines.filter(l => l[nthDigit] == targetDigit), nthDigit + 1)
+}
+
+const a = (await input_reader(import.meta.resolve))
+  .trim()
+  .split("\n")
+  .thrush((allLines) => [
+    finderProc(first, allLines),
+    finderProc(last, allLines),
+  ])
+  .map(s => parseInt(s, 2))
+  .thrush(([a, b]) => a * b)
+
+export default a;
+
+if (import.meta.main) {
+  console.log(a);
+}
