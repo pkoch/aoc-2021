9407b366eddb877f01d566862d5c125bdd54049c
Paulo Köch <paulo.koch@gmail.com>
2022-12-06 18:20:52 +0000

    Prepare for 3/2

--- 3/1.ts
+++ 3/1.ts
@@ -1,28 +1,15 @@
-import { assertNever, input_reader } from "../libtapete.ts";
+import { input_reader, transpose } from "../libtapete.ts";
 import "../langExts/Object/thrush.ts"
 
-const zip = <T>(...arrays: T[][]): T[][] => {
-  const result: T[][] = [];
-  for(const arr of arrays){
-    for(const [i, element] of arr.entries()) {
-      if(!result[i]) result[i] = [];
-      result[i].push(element);
-    }
-  }
-  return result;
-}
-
-const transpose = <T>(a: T[][]): T[][] => {
-  return zip(...a);
-}
-
-const tally = (entries: string[]): Record<string, number> => {
-  const result: Record<string, number> = {}
+export const tallyByFreq = (entries: string[]): string[] => {
+  const tally: Record<string, number> = {}
   for(const entry of entries){
-    result[entry] ||= 0;
-    result[entry]++;
+    tally[entry] ||= 0;
+    tally[entry]++;
   }
-  return result;
+  return Object.entries(tally)
+    .toSorted(([_vA, freqA], [_vB, freqB]) => freqA - freqB)
+    .map(([k, _v]) => k)
 }
 
 const a = (await input_reader(import.meta.resolve))
@@ -30,9 +17,7 @@ const a = (await input_reader(import.meta.resolve))
   .split("\n")
   .map(l => l.split(''))
   .thrush(transpose)
-  .map(tally)
-  .map(Object.entries)
-  .map(l => l.toSorted(([_vA, freqA], [_vB, freqB]) => freqA - freqB).map(([k, v]) => k))
+  .map(tallyByFreq)
   .thrush(transpose)
   .map(l => parseInt(l.join(''), 2))
   .thrush(([a, b]) => a * b)
--- libtapete.ts
+++ libtapete.ts
@@ -6,3 +6,18 @@ export const input_reader = async (resolve: (a: string) => string) =>
 export const assertNever = (o: unknown): never => {
   throw new Error(`Expected to be unreachable, got ${JSON.stringify(o)}`);
 };
+
+export const zip = <T>(...arrays: T[][]): T[][] => {
+  const result: T[][] = [];
+  for(const arr of arrays){
+    for(const [i, element] of arr.entries()) {
+      if(!result[i]) result[i] = [];
+      result[i].push(element);
+    }
+  }
+  return result;
+}
+
+export const transpose = <T>(a: T[][]): T[][] => {
+  return zip(...a);
+}
